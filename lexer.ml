6b3b00da1664e773c60bfafb4af14c03c60b8cb6
Julien Lou <julien@MacBook-Air-de-Julien.local>
2022-10-21 19:15:59 +0200

    Lexer correction

--- lexer.ml
+++ lexer.ml
@@ -5,13 +5,31 @@ type lexeme =
   |Right_parenthesis
 ;;
 
-let rec complete_word f n =
+let complete_word f n =
+  let rec get_number f1 n1 =
+    let len = String.length f1 in
+    if n1 >= len
+    then ""
+    else begin
+      match f1.[n1] with
+      |' ' -> ""
+      |'(' -> ""
+      |')' -> ""
+      |'+' -> ""
+      |_ -> (String.make 1 f1.[n1]) ^ (get_number f1 (n1 + 1))
+    end
+  in
   let len = String.length f in
-  if n >= len
+    if n >= len
     then ""
-  else if f.[n] = ' '
-    then ""
-  else (String.make 1 f.[n]) ^ complete_word f (n + 1)
+    else begin
+      match f.[n] with
+      |' ' -> " "
+      |'(' -> "("
+      |')' -> ")"
+      |'+' -> "+"
+      |_ -> get_number f n
+    end
 ;;
 
 let lexical_analysis f =
@@ -19,6 +37,7 @@ let lexical_analysis f =
   let rec aux index str =
     if index < len then match (complete_word str index) with
       |"" -> (aux (index + 1) str)
+      |" " -> (aux (index + 1) str)
       |"(" -> Left_parenthesis::(aux (index + 1) str)
       |")" -> Right_parenthesis::(aux (index + 1) str)
       |"+" -> Plus_sign::(aux (index + 1) str)
@@ -26,4 +45,3 @@ let lexical_analysis f =
     else []
   in aux 0 f
 ;;
-
--- parser.ml
+++ parser.ml
@@ -12,12 +12,16 @@ and full_expression incomplete_lexeme_list =
 let rec largest_independent_expression lexeme_list =
   let rec aux list_of_lexeme int_stack =
     match list_of_lexeme with
-      |[] -> Empty
+      |[] -> raise (Error "Parser error.")
       |h::[] -> 
         begin match h with
           |Integer(number) -> 
-            begin if int_stack = 0 then
-              [Integer(number)]
+            begin 
+              if int_stack = 0 then
+                [Integer(number)]
+              else 
+                raise (Error "Missing parentheses.")
+            end
           |Plus_sign -> raise (Error "The expression is incorrect.")
           |Left_parenthesis -> raise (Error "Missing parentheses.")
           |Right_parenthesis -> 
@@ -27,20 +31,28 @@ let rec largest_independent_expression lexeme_list =
               else 
                 raise (Error "Missing parentheses.")
             end
+        end
       |h::t -> 
         begin match h with
-          |Integer(number) -> [Integer(number)]  (* change here for multiplication*)
+          |Integer(number) ->
+            begin
+              if int_stack = 0 then
+                [Integer(number)]  (* change here for multiplication*)
+              else
+                [Integer(number)] @ (aux t int_stack)
+            end
           |Plus_sign -> [Plus_sign] @ (aux t int_stack)
           |Left_parenthesis -> [Left_parenthesis] @ (aux t (int_stack + 1))
           |Right_parenthesis -> 
             begin
               if int_stack <= 0 then
                 raise (Error "Too many right parentheses.")
-              if int_stack = 1 then
+              else if int_stack = 1 then
                 [Right_parenthesis]
               else
                 [Right_parenthesis] @ (aux t (int_stack - 1))
             end
+        end
   in aux lexeme_list 0
 ;;
         
